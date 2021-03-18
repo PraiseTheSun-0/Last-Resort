67607aa76163c5cb5b366982e06a890f92d12efe
human3562 <31441934+human3562@users.noreply.github.com>
2021-03-18 16:03:47 +0300

    Camera movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
     private int gold { get; set; }
     private int team { get; set; }
     //build
